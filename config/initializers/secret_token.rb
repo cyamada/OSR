f49febd96235bd07d9cab48e99ee7a52a03867eb
Connor Yamada <ckyamada@gmail.com>
2012-10-07 19:17:47 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osr::Application.config.secret_token = '56710a06d9979bbdaada92f9ff863da4eeaddca6fb42abcbba205302b7510414ce9be9995e2a184e756acb6d5cc1391bb802852ef63651facd9fcbac561b76f2'
